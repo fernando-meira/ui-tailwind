ebb3d1413e1e6c039927bb292f4d7eedbd6de077
Fernando Meira <fernando.meira@goomer.com.br>
2023-03-26 13:59:45 -0300

    feat: Adjusted page structure and started styling.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
-import './globals.css';
-
 import { Inter } from 'next/font/google';
+
+import { Sidebar } from '../components/Sidebar';
+import { Header } from '../components/Header';
+
+import './globals.css';
+import { HeroPattern } from '../components/HeroPattern';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +20,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <body className="bg-zinc-900">{children}</body>
+      <body className="bg-zinc-900">
+        <Sidebar />
+
+        <div className="ml-80 relative h-screen">
+          <Header />
+
+          <HeroPattern />
+
+          <div className="py-24 max-w-4xl px-8 mx-auto">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
